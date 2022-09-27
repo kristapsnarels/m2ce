ad744716c99129d32915e0caac505edcad298db6
kristapsnarels <kristaps.narels@pearlgroup.no>
2022-09-27 12:49:30 +0300

    TP-345 training module for frontend layout learning

---
+++ app/code/Convert/Layout/registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author Convert Team
+ * @copyright Copyright (c) Convert (https://www.convert.no)
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Convert_Layout', __DIR__);
