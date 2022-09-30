bb497f60dcf312587901bfb9604728fe9d906dbb
kristapsnarels <kristaps.narels@pearlgroup.no>
2022-09-30 15:13:09 +0300

    creating module structure and adding physical stock admin tab

---
+++ app/code/Convert/PhysicalStock/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Convert Team
+ * @copyright Copyright (c) Convert (https://www.convert.no)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Convert_PhysicalStock', __DIR__);
